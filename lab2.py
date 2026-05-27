fea9449ca751095206f4749af00a28d38f9ccdde
Brandon Ng <ngskbrandon08.25@ichat.sp.edu.sg>
2026-05-28 00:54:11 +0800

    yipe

--- lab2.py
+++ lab2.py
@@ -1,1 +1,26 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers seperated by commas")
+    
+def get_user_input():
+    user_input = input()
+    string_list = user_input.split(",")
+    float_list = []
+    for num_str in string_list:
+        float_list.append(float(num_str)) 
+        
+    return float_list
+
+def calculate_average_temperature(num_list):
+    total = sum(num_list)
+    average = total / len(num_list)
+    
+    return average
+
+def calculate_min_max_temperature(num_list):
+    minimum = min(num_list)
+    maximum = max(num_list)
+    
+    return [minimum, maximum]
+    
+    
+    
